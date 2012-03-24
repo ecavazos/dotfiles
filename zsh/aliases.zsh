0ab223bfffc3db8f70b95d9acd76f524fd1cb771
ecavazos <ejcavazos@gmail.com>
2012-03-23 19:24:08 -0700

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,22 +1,21 @@
-alias c="cd ~/code/alchemy"
-alias w="cd ~/code/work"
-alias d="cd ~/code/alchemy/dotfiles"
+alias c='cd ~/code/alchemy'
+alias w='cd ~/code/work'
+alias d='cd ~/code/alchemy/dotfiles'
 
-alias be="bundle exec"
-alias rs="be rspec"
+alias be='bundle exec'
+alias rs='be rspec'
 
-alias migrate="be rake db:migrate db:test:prepare"
-alias remigrate="be rake db:migrate && rake db:migrate:redo && rake db:schema:dump db:test:prepare"
-alias remongrate="be rake mongoid:migrate && rake mongoid:migrate:redo"
+alias migrate='be rake db:migrate db:test:prepare'
+alias remigrate='be rake db:migrate && rake db:migrate:redo && rake db:schema:dump db:test:prepare'
+alias remongrate='be rake mongoid:migrate && rake mongoid:migrate:redo'
 
-alias g="git"
-alias gadd="git add -u && git add . && git status"
-alias gpom="git push origin master"
-alias gci="git pull --rebase && rake && git push"
+alias g='git'
+alias gadd='git add -u && git add . && git status'
+alias gmm='g fetch origin && g branch -a --merged master'
 
-alias tlf="tail -f"
+alias tlf='tail -f'
 
-alias md="mongod run --config /usr/local/Cellar/mongodb/1.4.3-x86_64/mongod.conf"
+alias md='mongod run --config /usr/local/Cellar/mongodb/1.4.3-x86_64/mongod.conf'
 
 # -------------------------------------------------------------------------
 # profile shortcuts
