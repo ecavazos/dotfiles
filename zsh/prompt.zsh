10dffd1dca73a26f93c9fd6b1c60b11f1d52be9a
Emilio Cavazos <ejcavazos@gmail.com>
2012-12-03 09:09:49 -0800

    removing unicode chars from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,12 +4,13 @@ setopt prompt_subst
 function ruby_version() {
   if type rbenv > /dev/null; then
     ver=$(rbenv version | sed 's/[  ].*$//')
-    clr=$fg_bold[cyan]
+    clr=$fg_bold[gray]
     echo "%{$clr%}$ver%{$reset_color%}"
   fi
 }
 
 # set prompt
-PROMPT='%{$reset_color%}%{$fg[blue]%}✖ %{$reset_color%}'
+PROMPT='%{$reset_color%}%{$fg_bold[red]%}%~
+%{$fg[magenta]%}-- %{$reset_color%}'
 RPROMPT='`ruby_version``git-prompt`%{$reset_color%}'
 
