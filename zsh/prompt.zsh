a55bc49c22fb784c0974a5d9b928f2efc262ed3e
Emilio Cavazos <ejcavazos@gmail.com>
2013-03-22 22:53:14 -0700

    remove prompt cruft

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,12 +4,25 @@ setopt prompt_subst
 function ruby_version() {
   if type rbenv > /dev/null; then
     ver=$(rbenv version | sed 's/[  ].*$//')
-    clr=$fg_bold[gray]
+    clr=$fg[gray]
     echo "%{$clr%}$ver%{$reset_color%}"
   fi
 }
 
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' get-revision true
+zstyle ':vcs_info:*' unstagedstr '%F{red}[u]'
+zstyle ':vcs_info:*' stagedstr '%F{blue}[s]'
+zstyle ':vcs_info:*' formats "%F{yellow}%b %m%u%c"
+zstyle ':vcs_info:*' enable git
+
+precmd() {
+  vcs_info
+}
+
 # set prompt
-PROMPT='%{$fg_bold[magenta]%}%~ %{$fg_bold[green]%}$ %{$reset_color%}'
-RPROMPT='`ruby_version``git-prompt`%{$reset_color%}'
+PROMPT='%{$fg[magenta]%}%~ %{$fg[green]%}$%{$reset_color%} '
+RPROMPT='${vcs_info_msg_0_}%{$reset_color%}'
 
