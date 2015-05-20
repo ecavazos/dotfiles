dfcd2a7a636bdb0fb8f6196e017d70e9e92a1dc4
Emilio Cavazos <ejcavazos@gmail.com>
2015-05-19 21:51:10 -0700

    alias; bg=none

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias c="cd ~/code"
-alias d="vim ~/code/dotfiles"
+alias d="$EDITOR ~/code/dotfiles"
+alias dt="cd ~/code/dotfiles"
 
 alias be="bundle exec"
 alias rs="be rspec"
@@ -18,6 +19,9 @@ alias gmm="g fetch origin && g branch -a --merged master"
 alias tlf="tail -f"
 
 alias ls="ls --color=auto"
+
+# Attach to a tmux session and detach other clients
+alias ta="tmux a -d"
 
 if [[ `uname` == "Darwin" ]]; then
   alias ls="ls -G"
