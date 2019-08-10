4e96c00bddb1ada9aa90fca414cb5254e5748ccd
Emilio Cavazos <ejcavazos@gmail.com>
2019-08-10 08:58:29 -0700

    update PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,8 @@
 export PATH=$HOME/.bin:$PATH
 export PATH=$HOME/.rbenv/bin:$PATH
-export PATH=/usr/local/mysql/bin:$PATH
+export PATH=$HOME/local/bin:$PATH
 export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/mysql/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 
 export GOPATH=$HOME/code/go
