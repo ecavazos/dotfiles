3882a80799957e0ba517d561e9872b7436b7aa34
Emilio Cavazos <ejcavazos@gmail.com>
2024-12-23 19:59:33 -0800

    add neovide settings

--- bashrc
+++ bashrc
@@ -1,7 +1,8 @@
 # ...full config
 
-alias nvim-chad='NVIM_APPNAME=nvim-chad nvim'
+alias chad='NVIM_APPNAME=nvim-chad neovide'
 alias cat='bat'
-alias notes='cd ~/Sync/Notes/ && nvim-chad'
-alias g="git"
+alias notes='cd ~/Sync/Notes/ && chad'
+alias g='git'
+alias dots='cd ~/Laptop\ Sync/code/dotfiles/ && chad'
 
--- configs/nvim-chad/lua/options.lua
+++ configs/nvim-chad/lua/options.lua
@@ -14,3 +14,22 @@ vim.opt.linebreak = true
 
 vim.g.markdown_recommended_style = 0
 
+-- Neovide Settings --
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.95
+
+  vim.g.neovide_padding_top = 20
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 10
+  vim.g.neovide_padding_left = 15
+
+  -- vim.keymap.set({'n', 'v'}, '<C-c>', '"+y', {noremap = true})
+  -- vim.keymap.set({'n', 'v'}, '<C-v>', '"+p', {noremap = true})
+  vim.api.nvim_set_keymap('v', '<sc-c>', '"+y', {noremap = true})
+	vim.api.nvim_set_keymap('n', '<sc-v>', 'l"+P', {noremap = true})
+	vim.api.nvim_set_keymap('v', '<sc-v>', '"+P', {noremap = true})
+	vim.api.nvim_set_keymap('c', '<sc-v>', '<C-o>l<C-o>"+<C-o>P<C-o>l', {noremap = true})
+	vim.api.nvim_set_keymap('i', '<sc-v>', '<ESC>l"+Pli', {noremap = true})
+	vim.api.nvim_set_keymap('t', '<sc-v>', '<C-\\><C-n>"+Pi', {noremap = true})
+end
+
